4e3b15c6b366575ba2f3d65fcc4cdc00f8f59133
Trung Hieu <xhieuit@gmail.com>
2022-12-23 10:38:28 +0700

    xong state them nguoi dung

--- src/component/Admin/Content/ModalCreatUser.js
+++ src/component/Admin/Content/ModalCreatUser.js
@@ -12,7 +12,19 @@ const ModalCreatUser = () => {
     const [email, setEmail] = useState("")
     const [password, setPassword] = useState("")
     const [username, setUsername] = useState("")
+    const [role, setRole] = useState("ADMIN")
     const [image, setImage] = useState("")
+    const [previewImage, setPreviewImage] = useState("")
+
+    const handleUploadImage = (event) => {
+        if (event.target && event.target.files && event.target.files[0]) {
+            setPreviewImage(URL.createObjectURL(event.target.files[0]))
+            setImage(event.target.files[0])
+        } else {
+            setPreviewImage("")
+        }
+    }
+
     return (
         <>
             <Button variant="primary" onClick={handleShow}>
@@ -34,32 +46,63 @@ const ModalCreatUser = () => {
                     <form className="row g-3">
                         <div className="col-md-6">
                             <label className="form-label">Email</label>
-                            <input type="email" className="form-control" value={email} />
+                            <input
+                                type="email"
+                                className="form-control"
+                                value={email}
+                                onChange={e => setEmail(e.target.value)}
+                            />
                         </div>
                         <div className="col-md-6">
                             <label className="form-label">Password</label>
-                            <input type="password" className="form-control" value={password} />
+                            <input
+                                type="password"
+                                className="form-control"
+                                value={password}
+                                onChange={e => setPassword(e.target.value)}
+                            />
                         </div>
                         <div className="col-md-6">
                             <label className="form-label">Username</label>
-                            <input type="text" className="form-control" value={username} />
+                            <input
+                                type="text"
+                                className="form-control"
+                                value={username}
+                                onChange={e => setUsername(e.target.value)}
+                            />
                         </div>
                         <div className="col-md-4">
                             <label className="form-label">Role</label>
-                            <select className="form-select">
-                                <option selected value="USER">USER</option>
-                                <option selected value="ADMIN">ADMIN</option>
+                            <select
+                                value={role}
+                                className="form-select"
+                                onChange={e => setRole(e.target.value)}
+                            >
+                                <option value="USER">USER</option>
+                                <option value="ADMIN">ADMIN</option>
                             </select>
                         </div>
                         <div className="col-md-12">
-                            <label className="form-label label-upload" htmlFor="labelUpload">
+                            <label
+                                className="form-label label-upload"
+                                htmlFor="labelUpload"
+                            >
                                 <FcPlus /> Upload file Image
                             </label>
-                            <input id="labelUpload" type="file" hidden />
+                            <input
+                                id="labelUpload"
+                                type="file"
+                                hidden
+                                onChange={event => handleUploadImage(event)}
+                            />
                         </div>
                         <div className="col-md-12 img-preview">
-                            {/* <span>Preview Image</span> */}
-                            <img src="https://bit.ly/eric-bot-2" />
+                            {previewImage ?
+                                <img src={previewImage} alt="img" />
+                                // < img src="https://bit.ly/eric-bot-2" />
+                                :
+                                <span>Preview Image</span>
+                            }
                         </div>
 
                     </form>
